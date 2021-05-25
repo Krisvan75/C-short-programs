80b1282808d883e626b0116b9a2b111007b0a8d1
Krisvan75 <33301689+Krisvan75@users.noreply.github.com>
2021-05-24 20:37:36 -0700

    Update Q_Func.py

--- Q_Func.py
+++ Q_Func.py
@@ -1,1 +1,18 @@
+import numpy as np
+from scipy import special as sp
+import math
 
+def Qfunc(f):
+    return 0.5-0.5*sp.erf(f/math.sqrt(2))
+
+def inv_Qfunc(f):
+    return math.sqrt(2)*sp.erfcinv(2*f)
+
+f = float(input("y:"))
+mode = input('mode:(Q or invQ)')
+
+if mode == 'Q':
+    print(Qfunc(f))
+
+elif mode == 'invQ':
+    print(inv_Qfunc(f))
